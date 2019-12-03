dc9e9bf65a11f06fa1dfbbf2527cb7a1ea471348
Paul <paulereverend@gmail.com>
2019-12-03 14:31:34 +0100

    Fixed translation

--- src/main/java/com/paullereverend/exceptions/UtilisateurNotFoundException.java
+++ src/main/java/com/paullereverend/exceptions/UtilisateurNotFoundException.java
@@ -3,6 +3,6 @@ package com.paullereverend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Utilisateur not found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Utilisateur non trouvé")
 public class UtilisateurNotFoundException extends RuntimeException {
 }
